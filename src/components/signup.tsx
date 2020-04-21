bc8971e4603b56656d1339bc2483d59d81eaad20
Triple0 <lekan.olapeju@yahoo.com>
2020-04-21 18:28:09 +0100

    Added dummy imports into files

---
+++ src/components/signup.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../store';
+import { Grid, Form, Input, Button } from 'semantic-ui-react';
---
+++ src/store/reducers/loginReducer.ts
@@ -0,0 +1,6 @@
+import {
+    TaskState,
+    DELETE_TASK_FROM_LIST,
+    ADD_TASK_TO_LIST,
+    TaskActionTypes,
+  } from "./types";
