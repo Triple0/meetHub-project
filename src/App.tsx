76ba210f5a895bc6fd1cec0a8bb6a04b5313cac1
Triple0 <lekan.olapeju@yahoo.com>
2020-04-26 02:58:56 +0100

    added NavBar and worked on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import meetHubLogo from './logo/meetHubLogo.png';
 import './App.css';
-import NewsForm  from './components/NewsForm';
+import NewsForm from './components/NewsForm';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import { Grid } from 'semantic-ui-react';
+import NavBar from './components/NavBar';
 
 
 export interface IAppProps {
@@ -17,17 +18,30 @@ export default class App extends React.Component<IAppProps> {
     return (
       <Grid centered>
         <Router>
-        <Switch>
-          <Route path="/NewsForm" />
-          <Route path="/Profiles" />
-        </Switch>
+          <Switch>
+            <Route path="/NewsForm" />
+            <Route path="/Profiles" />
+          </Switch>
         </Router>
-        <div className="App">
+        <Grid.Row centered>
           <header className="App-header">
             <img src={meetHubLogo} className="App-logo" alt="logo" />
+
           </header>
+
+        </Grid.Row>
+        <Grid.Row centered>
+
+          <NavBar />
+        </Grid.Row>
+
+        <Grid.Row centered>
           <NewsForm />
-        </div>
+        </Grid.Row>
+
+
+
+
 
       </Grid>
     );
--- src/components/NewsForm.tsx
+++ src/components/NewsForm.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../store';
-import { Grid, Button } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { NewsFeed } from '../store/types/NewsFeed';
 import NewsDisplay from './NewsDisplay';
-import { Link } from 'react-router-dom';
+
 
 
 export interface INewsFormProps {
@@ -18,9 +18,6 @@ export class NewsForm extends React.Component<INewsFormProps> {
 
             <Grid centered>
                 <Grid.Row centered>
-                <Button as={Link} to={"/Profile"} contact="GoToProfilePage" color="teal" fluid size="large">
-            GoToLoginPage
-          </Button>
                     <h1>NewsFeed</h1>
                 </Grid.Row>
 
